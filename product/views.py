92185b67a52ba193218baa64c4c4645263742b32
Srejus Jayakumar <82509096+srejus@users.noreply.github.com>
2023-06-15 19:36:28 +0530

    Bug fixes and removed unwanted import

--- product/views.py
+++ product/views.py
@@ -1,7 +1,5 @@
 import requests
 
-from django.shortcuts import render
-
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
--- recommendation/views.py
+++ recommendation/views.py
@@ -19,7 +19,11 @@ class RecommendationView(APIView):
         if not user.exists():
             return Response({"statius":"failed","message":"No User found with the given id"},status=status.HTTP_404_NOT_FOUND)
         
-        result = recommend_products(user.first())
+        try:
+            result = recommend_products(user.first())
+        except ValueError as e:
+            return Response({"status":"failed","message":str(e)},status=status.HTTP_400_BAD_REQUEST)
+        
         actual_products = [item.product for item in OrderItem.objects.filter(order__customer=user.first())]
 
         accuracy = calculate_accuracy(actual_products, result)
